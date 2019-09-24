edceb11cfc7fa94a222a7722f1963f8c8483924b
rmdanwang <rm.danwang@gmail.com>
2019-09-23 21:31:37 -0500

    updated

--- main.tf
+++ main.tf
@@ -4,9 +4,15 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
+resource "google_compute_instance" "svc-1" {
+  name         = "wang9847-testvm1"
   machine_type = "f1-micro"
+  zone         = "us-central1-c"
+
+  metadata = {
+    ssh-keys = "INSERT_USERNAME:${file("~/.ssh/google_compute_engine.pub")}"
+    #startup_script = "echo hi > /test.txt" # doesn't work
+   }
 
   boot_disk {
     initialize_params {
@@ -15,14 +21,69 @@ resource "google_compute_instance" "vm_instance" {
   }
 
   network_interface {
-    # A default network is created for all GCP projects
-    network       = "default"
+    network = "default"
+
     access_config {
+      // Ephemeral IP
     }
   }
+
+  provisioner "remote-exec" {
+    connection {
+      host        = "${google_compute_instance.svc-1.network_interface.0.access_config.0.nat_ip}"
+      user        = "wang9847"
+      type        = "ssh"
+      private_key = "${file("~/.ssh/google_compute_engine")}"
+      }
+    inline = [
+      "mkdir -p ~/svc-01/html",
+    ]
+  }
+
+  provisioner "file" {
+    source      = "svc-01/"
+    destination = "~/svc-01"
+    connection {
+      host        = "${google_compute_instance.svc-1.network_interface.0.access_config.0.nat_ip}"
+      user        = "betz4871"
+      type        = "ssh"
+      private_key = "${file("~/.ssh/google_compute_engine")}"
+    }
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      host        = "${google_compute_instance.svc-1.network_interface.0.access_config.0.nat_ip}"
+      user        = "betz4871"
+      type        = "ssh"
+      private_key = "${file("~/.ssh/google_compute_engine")}"
+      }
+    inline = [ 
+      "chmod +x ~/svc-01/install.sh",
+      "sudo ~/svc-01/install.sh",
+    ]
+  }
+
+  #metadata_startup_script = "echo hello > ~/success.txt"
+  
+  #service account is essential for file provisioner
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+
 }
 
-resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
-  auto_create_subnetworks = "true"
+resource "google_compute_firewall" "default" {
+ name    = "svc01-firewall"
+ network = "default"
+
+ allow {
+   protocol = "tcp"
+   ports    = ["80"]
+ }
 }
+
+  output "ip" {
+     value = "${google_compute_instance.svc-1.network_interface.0.access_config.0.nat_ip}"
+  }
+
